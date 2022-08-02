efeac905763ef52fa54bc739cc1164af83c8944b
Shivansh Srivastava <srivastavashiv0112@gmail.com>
2022-08-02 23:42:13 +0530

    CLI aff

---
+++ MyClass.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class MyClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        int b = sc.nextInt();
+        int c = 0;
+        while (sum(a) > b) {
+            a++;
+            c++;
+        }
+        System.out.println(c);
+    }
+
+    public static long sum(long a) {
+        long s = 0;
+        while (a > 0) {
+            s = s + a % 10;
+            a = a / 10;
+        }
+        return (s);
+    }
+}
